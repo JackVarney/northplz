1ef325409fe39f7482e1b9fb3badf79e2596fefb
Jack Varney <jackvarney@live.com>
2022-04-01 06:00:18 +0100

    This will actually have to do for now

--- src/background-renderer/background-renderer.ts
+++ src/background-renderer/background-renderer.ts
@@ -4,10 +4,10 @@ import { createNumberRotator, hsla } from "../util";
 export default async () => {
   const defaultBackground = await images.defaultBackground;
 
-  const hue_min = 280;
-  const hue_max = 320;
+  const hue_min = 0;
+  const hue_max = 360;
   const increment = 1;
-  const interval = 1;
+  const interval = 20;
   const hueRotator = createNumberRotator(
     hue_min,
     hue_max,
@@ -17,7 +17,7 @@ export default async () => {
   );
 
   const render = (ctx: CanvasRenderingContext2D) => {
-    ctx.globalAlpha = 0.2;
+    ctx.globalAlpha = 0.05;
 
     ctx.drawImage(
       defaultBackground,
@@ -27,7 +27,7 @@ export default async () => {
       window.innerHeight
     );
 
-    ctx.fillStyle = hsla(hueRotator(), 100, 20, 0.2);
+    ctx.fillStyle = hsla(hueRotator(), 100, 20, 0.05);
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
   };
 
